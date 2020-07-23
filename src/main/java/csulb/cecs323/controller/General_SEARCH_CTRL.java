7447bb410cb7f589742e1d964559e60ff5644f45
William Duong <35279699+w-duong@users.noreply.github.com>
2020-07-23 13:14:16 -0700

    shortcut functionality added to General search screen

--- src/main/java/csulb/cecs323/controller/General_SEARCH_CTRL.java
+++ src/main/java/csulb/cecs323/controller/General_SEARCH_CTRL.java
@@ -9,11 +9,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import javax.swing.*;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -39,14 +45,17 @@ public class General_SEARCH_CTRL<Type> implements Initializable
         resultsListView.setItems(resultsList);
     }
 
-    public void searchOnSelectButton (ActionEvent actionEvent)
+    public void onSearchButton (ActionEvent actionEvent) { searchOperation(); }
+
+    public void onEnterKey (KeyEvent keyEvent)
     {
-        resultsList.removeAll(resultsList);
-        searchOperation();
+        if (keyEvent.getCode().equals(KeyCode.ENTER))
+            searchOperation();
     }
 
     public void searchOperation ()
     {
+        resultsList.removeAll(resultsList);
         String searchString = inputSearchBar.getText();
         TypedQuery query;
 
@@ -60,5 +69,28 @@ public class General_SEARCH_CTRL<Type> implements Initializable
             query = entityManager.createNamedQuery(Drug.FIND_ALL_BY_NAME, Drug.class).setParameter("searchString", searchString);
             resultsList.addAll(query.getResultList());
         }
+    }
+
+    public void selectOperation ()
+    {
+        workingCopy = resultsListView.getSelectionModel().getSelectedItem();
+
+        Stage popUp = (Stage) inputSearchBar.getScene().getWindow();
+        popUp.close();
+    }
+
+    public void onSelectButton (ActionEvent actionEvent) { selectOperation(); }
+
+    KeyCombination select_shortcut = new KeyCodeCombination(KeyCode.T, KeyCombination.ALT_DOWN);
+    public void onSelectShortcut (KeyEvent keyEvent)
+    {
+        if (select_shortcut.match(keyEvent))
+            selectOperation();
+    }
+
+    public void onCancelButton (ActionEvent actionEvent)
+    {
+        Stage popUp = (Stage) inputSearchBar.getScene().getWindow();
+        popUp.close();
     }
 }
